9b9bf12c616f200a94647f4fa2878d253ec195ea
RodrigoLlanes <romadritex@gmail.com>
2021-10-26 15:33:06 +0200

    Day 02

--- Day 02/Sol1.py
+++ Day 02/Sol1.py
@@ -4,13 +4,13 @@ from collections import Counter
 def solve(ids):
     two = 0
     three = 0
-    for id1 in ids:
-        for id2 in ids:
-            dif = [char1 for (char1, char2) in zip(id1, id2) if char1 == char2]
-            if len(dif) == len(id1)-1:
-                return "".join(dif)
+    for id in ids:
+        c = Counter(id)
+        two += (2 in c.values())
+        three += (3 in c.values())
+    return two * three
 
 
 if __name__ == "__main__":
-    ids = [frec.strip() for frec in open("input.txt", "r").readlines()]
+    ids = [id.strip() for id in open("input.txt", "r").readlines()]
     print(solve(ids))
